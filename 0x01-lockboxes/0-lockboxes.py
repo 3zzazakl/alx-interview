13dfa2215cb3f25322d62efd903e5d05f475948b
3zzazakl <vanzekovichgr@gmail.com>
2024-12-07 21:07:36 +0200

    fixing pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,23 +6,21 @@ argument -- description
 Return: return_description
 """
 
+
 def canUnlockAll(boxes):
     """sumary_line
-    
+
     Keyword arguments:
     argument -- description
     Return: return_description
     """
-    
     unlocked = set([0])
-    
     to_process = list(unlocked)
-    
     while to_process:
         current_box = to_process.pop()
         for key in boxes[current_box]:
             if key < len(boxes) and key not in unlocked:
                 unlocked.add(key)
                 to_process.append(key)
-    
+
     return len(unlocked) == len(boxes)
