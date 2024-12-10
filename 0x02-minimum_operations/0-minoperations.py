bd915f2952eb255205d16b656d2965378a89754c
3zzazakl <vanzekovichgr@gmail.com>
2024-12-10 08:11:45 +0200

    checking task0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,17 +11,10 @@ def minOperations(n):
     """
     Description
     """
-    if n == 1:
-        return 1
+    if n <= 1:
+        return 0
 
-    operations = 0
     for i in range(2, n + 1):
         if n % i == 0:
-            operations += 1
-            n = n // i
-            break
-
-    if n > 1:
-        operations += n
-
-    return operations
+            return i + minOperations(n // i)
+    return n
