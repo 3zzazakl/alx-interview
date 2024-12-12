5b9c4ee57efd165c7c2ae825d9720e28c05d1640
3zzazakl <vanzekovichgr@gmail.com>
2024-12-12 18:01:12 +0200

    setting style

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,18 +2,18 @@
 
 const request = require('request');
 
-function getCharacters(urls) {
-    if (!Array.isArray(urls) || urls.length === 0) return;
-    request.get(urls[0], (_err, _res, body) => {
-        if (_err) return console.error(_err);
-        console.log(JSON.parse(body).name);
-        getCharacters(urls.slice(1));
-    });
+function getCharacters (urls) {
+  if (!Array.isArray(urls) || urls.length === 0) return;
+  request.get(urls[0], (_err, _res, body) => {
+    if (_err) return console.error(_err);
+    console.log(JSON.parse(body).name);
+    getCharacters(urls.slice(1));
+  });
 }
 
 request.get(
-    'https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (_err, _res, body) => {
-        const characters = JSON.parse(body).characters;
-        getCharacters(characters);
-    }
+  'https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (_err, _res, body) => {
+    const characters = JSON.parse(body).characters;
+    getCharacters(characters);
+  }
 );
