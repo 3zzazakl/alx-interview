1ccc8b835e9065d6319a9485569f41d2821e7942
3zzazakl <vanzekovichgr@gmail.com>
2024-12-12 18:29:14 +0200

    checking style

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -23,16 +23,16 @@ def validUTF8(data):
                 return False
             i += 2
         elif byte & 0xF0 == 0xE0:
-            if i + 2 >= n or
-            data[i + 1] & 0xC0 != 0x80 or
-            data[i + 2] & 0xC0 != 0x80:
+            if (i + 2 >= n or
+                data[i + 1] & 0xC0 != 0x80 or
+                data[i + 2] & 0xC0 != 0x80):
                 return False
             i += 3
         elif byte & 0xF8 == 0xF0:
-            if i + 3 >= n or
-            data[i + 1] & 0xC0 != 0x80 or
-            data[i + 2] & 0xC0 != 0x80 or
-            data[i + 3] & 0xC0 != 0x80:
+            if (i + 3 >= n or
+                data[i + 1] & 0xC0 != 0x80 or
+                data[i + 2] & 0xC0 != 0x80 or
+                data[i + 3] & 0xC0 != 0x80):
                 return False
             i += 4
         else:
